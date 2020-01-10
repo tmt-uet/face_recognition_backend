ab363fd88d4991b5f2eea46b844dc815776f2b16
tmt98 <tungmin2410@gmail.com>
2020-01-10 17:08:50 +0700

    fix

--- test.py
+++ test.py
@@ -1,50 +1,53 @@
 import face_recognition
-from numpy import save
-from numpy import load
-import os
-from my_sql_db import Database
-# known_image = face_recognition.load_image_file("/home/tmt/Documents/face_recognition/my_app/storage/beauty_20191009001401_test.jpg")
-# biden_encoding = face_recognition.face_encodings(known_image)[0]
-# print(biden_encoding)
-# save('/home/tmt/Documents/face_recognition/my_app/storage/trained/data.npy', biden_encoding)
-
-
-# data = load('/home/tmt/Documents/face_recognition/my_app/storage/trained/data.npy')
-# print(data)
-# os.remove('/home/tmt/Documents/face_recognition/my_app/storage/trained/beauty_20190915092027.jpg')
-# /home/tmt/Documents/face_recognition/my_app/storage/beauty_20190915092027.jpg
-# db = Database()
-# result = db.select('SELECT id,name FROM users WHERE name = %s', ['Trang'])
-# print(type(result[0][1]))
-# print(result[0][0])
-
-
-import face_recognition
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import time
-start_time = time.time()
 
-known_image = face_recognition.load_image_file("/home/tmt/Documents/face_recognition/my_app/storage")
-# unknown_image = face_recognition.load_image_file("/home/tmt/Documents/face/collection/TMT/IMG_20190912_172925.jpg")
-unknown_image = face_recognition.load_image_file("/home/tmt/Documents/face/collection/Cong Anh/43358318_1119791154838067_3691811964742270976_n.jpg")
+# img = Image.open('/home/tmt/Documents/face_recognition/my_app/storage/trained/Tung/1578372281beauty_20191009001401.jpg').convert('LA')
+# img.save('/home/tmt/Documents/face_recognition/my_app/storage/trained/1578372281beauty_20191009001401.png')
 
-print('location')
-tung_face_location = face_recognition.face_locations(known_image, number_of_times_to_upsample=1)
-unknown_face_location = face_recognition.face_locations(unknown_image, number_of_times_to_upsample=1)
+import cv2
 
-print('encoding')
-# tung_encoding = face_recognition.face_encodings(known_image, known_face_locations=tung_face_location)[0]
-# unknown_encoding = face_recognition.face_encodings(unknown_image, known_face_locations=unknown_face_location)[0]
+# originalImage = cv2.imread('/home/tmt/Documents/face_recognition/my_app/storage/trained/Tung/1578372281beauty_20191009001401.jpg')
+# grayImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
 
-tung_encoding = face_recognition.face_encodings(known_image, known_face_locations=tung_face_location)[0]
-unknown_encoding = face_recognition.face_encodings(unknown_image, known_face_locations=unknown_face_location)[0]
+# (thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 127, 255, cv2.THRESH_BINARY)
 
-# results = face_recognition.face_distance([tung_encoding], unknown_encoding)
-# print(results)
-# print(face_recognition.compare_faces([tung_encoding], unknown_encoding, tolerance=0.56))
-# print("--- %s seconds ---" % (time.time() - start_time))
+# cv2.imshow('Black white image', blackAndWhiteImage)
+# cv2.imshow('Original image', originalImage)
+# cv2.imshow('Gray image', grayImage)
+
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+# cv2.imwrite('/home/tmt/Documents/face_recognition/my_app/storage/trained/1578372281beauty_20191009001401.jpg', grayImage)
 
 
-        if(face_distance_average > 1.68):
-            output['conclusion'] = json.dumps({"message": "Invalid", "face_distance_average": face_distance_average})
-        else:
-            output['conclusion'] = json.dumps({"message": "Valid", "face_distance_average": face_distance_average})
+# def rgb2gray(rgb):
+#     return np.dot(rgb[...,:3], [0.299, 0.587, 0.144])
+
+# img = mpimg.imread('lena.png')
+
+# gray = rgb2gray(img)
+
+# plt.imshow(gray, cmap = plt.get_cmap('gray'))
+
+# plt.savefig('lena_greyscale.png')
+# plt.show()
+
+known_image = face_recognition.load_image_file('/home/tmt/Documents/face_recognition/my_app/storage/trained/1578372281beauty_20191009001401.jpg')
+
+created1 = int(time.time())
+known_face_location = face_recognition.face_locations(known_image, number_of_times_to_upsample=1, model='cnn')
+known_encoding = face_recognition.face_encodings(known_image, known_face_locations=known_face_location)[0]
+created2 = int(time.time())
+print(created2-created1)
+
+known_image2 = face_recognition.load_image_file('/home/tmt/Documents/face_recognition/my_app/storage/trained/Tung/1578372281beauty_20191009001401.jpg')
+
+created3 = int(time.time())
+known_face_location2 = face_recognition.face_locations(known_image2, number_of_times_to_upsample=1, model='cnn')
+known_encoding2 = face_recognition.face_encodings(known_image, known_face_locations=known_face_location2)[0]
+created4 = int(time.time())
+print(created4-created3)
--- test_db.py
+++ test_db.py
@@ -3,19 +3,24 @@ from os import path
 import numpy as np
 import requests
 from my_sql_db import Database
-db = Database()
-name = 'abc'
-# results = db.select(
-#     'SELECT users.id, users.name, users.created, faces.id, faces.user_id, faces.filename,faces.created FROM users LEFT JOIN faces ON faces.user_id = users.id WHERE users.name = %s',
-#     [name])
-# re = db.select('SELECT id from users WHERE name= %s', [name])[0][0]
-# # print(results[0][5])
-# print(results)
-check_exist = db.select('SELECT count(*) from users WHERE name=%s', [name])
-# print(check_exist)
-check = db.select('SELECT * from users WHERE name=%s', [name])
-if (len(check)) > 0:
-    print("YES")
-print('len check', len(check))
+import shutil
 
-print("check exist", check_exist)
+# db = Database()
+# db.init_db()
+# name = 'abc'
+# # results = db.select(
+# #     'SELECT users.id, users.name, users.created, faces.id, faces.user_id, faces.filename,faces.created FROM users LEFT JOIN faces ON faces.user_id = users.id WHERE users.name = %s',
+# #     [name])
+# # re = db.select('SELECT id from users WHERE name= %s', [name])[0][0]
+# # # print(results[0][5])
+# # print(results)
+# check_exist = db.select('SELECT count(*) from users WHERE name=%s', [name])
+# # print(check_exist)
+# check = db.select('SELECT * from users WHERE name=%s', [name])
+# if (len(check)) > 0:
+#     print("YES")
+# print('len check', len(check))
+
+# print("check exist", check_exist)
+
+shutil.rmtree('/home/tmt/Documents/face/collection/Agent 1')
